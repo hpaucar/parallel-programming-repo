d985fc6cc67993854d62b16a04e004dd6d319ad3
Herminio <hpc424@gmail.com>
2020-02-04 20:59:19 -0500

    upload new Average exercises

---
+++ Laboratory/prog05.3_NumbersAvg/Debug/src/avg.d
@@ -0,0 +1,6 @@
+src/avg.o: ../src/avg.c /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
--- Laboratory/prog05.3_NumbersAvg/Debug/src/subdir.mk
+++ Laboratory/prog05.3_NumbersAvg/Debug/src/subdir.mk
@@ -3,21 +3,21 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CPP_SRCS += \
-../src/FirstMPIProject.cpp 
+C_SRCS += \
+../src/avg.c 
 
 OBJS += \
-./src/FirstMPIProject.o 
+./src/avg.o 
 
-CPP_DEPS += \
-./src/FirstMPIProject.d 
+C_DEPS += \
+./src/avg.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.cpp
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	mpic++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C Compiler'
+	mpicc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
